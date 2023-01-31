2c2ac722e76de18babf26f2f01b4faf497165c68
Yongjae Lim <yongjae@vt.edu>
2023-01-31 11:27:49 +0900

    changed widget type for the chatting list for future implementation and added some UI structures for the chatting list

--- lib/ui/chatting_list/chatting_list_view.dart
+++ lib/ui/chatting_list/chatting_list_view.dart
@@ -1,19 +1,34 @@
+import 'package:emo_chat_mobile/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChattingListView extends StatelessWidget {
+class ChattingListView extends StatefulWidget {
   const ChattingListView({Key? key}) : super(key: key);
 
   @override
+  State<ChattingListView> createState() => _ChattingListViewState();
+}
+
+class _ChattingListViewState extends State<ChattingListView> {
+  @override
   Widget build(BuildContext context) {
-    var userList = ['김파수', '이파수', '박파수'];
-    var userProfilePics = ['assets/images/1.png', 'assets/images/2.png', 'assets/images/3.png'];
+    var userList = ['김민욱', '이제훈', '박명수'];
+    var userProfilePics = [
+      'assets/images/1.png',
+      'assets/images/2.png',
+      'assets/images/3.png'
+    ];
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: false,
         title: const Text(
           'Chatting',
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 30
+          ),
         ),
         backgroundColor: Colors.white,
         actions: [
@@ -43,12 +58,52 @@ class ChattingListView extends StatelessWidget {
                 },
               );
             },
-            icon: const Icon(
-              Icons.logout
-            ),
+            icon: const Icon(Icons.logout),
             color: Colors.black,
           ),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: userList.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                      builder: (context) =>
+                          const MyHomePage(title: "Chatting Test View")));
+            },
+            child: Card(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(userProfilePics[index]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Column(
+                      children: [
+                        Text(
+                          userList[index],
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w100,
+                              color: Colors.black),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
